0f3b87747f78853a46df82e79e69814dd9bf3ee7
Viktoriia Krat <flightsspb@gmail.com>
2021-07-26 12:31:42 +0300

    resolve homework 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {PostService} from "./services/post.service";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Homework 1';
+
+
+  constructor() {
+  }
+}
